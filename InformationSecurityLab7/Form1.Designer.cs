0402a381e9b5a4d9ac12347bce2fa80bc50c9fdb
Maxim Lyubimov <maxim.lubimow2014@yandex.ru>
2024-12-03 21:58:59 +0400

    Добавьте файлы проекта.

---
+++ InformationSecurityLab7/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace InformationSecurityLab7
+{
+    partial class GronsfeldCipherForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDecrypted = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.buttonCypher = new System.Windows.Forms.Button();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDecrypted
+            // 
+            this.textBoxDecrypted.Location = new System.Drawing.Point(12, 12);
+            this.textBoxDecrypted.Multiline = true;
+            this.textBoxDecrypted.Name = "textBoxDecrypted";
+            this.textBoxDecrypted.Size = new System.Drawing.Size(299, 95);
+            this.textBoxDecrypted.TabIndex = 0;
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Location = new System.Drawing.Point(317, 12);
+            this.textBoxEncrypted.Multiline = true;
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(299, 95);
+            this.textBoxEncrypted.TabIndex = 1;
+            // 
+            // buttonCypher
+            // 
+            this.buttonCypher.Location = new System.Drawing.Point(258, 113);
+            this.buttonCypher.Name = "buttonCypher";
+            this.buttonCypher.Size = new System.Drawing.Size(109, 46);
+            this.buttonCypher.TabIndex = 2;
+            this.buttonCypher.Text = "Шифровать";
+            this.buttonCypher.UseVisualStyleBackColor = true;
+            this.buttonCypher.Click += new System.EventHandler(this.buttonCypher_Click);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(12, 127);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(119, 20);
+            this.textBoxKey.TabIndex = 3;
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(137, 130);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(33, 13);
+            this.labelKey.TabIndex = 4;
+            this.labelKey.Text = "Ключ";
+            // 
+            // GronsfeldCipherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 168);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.buttonCypher);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.textBoxDecrypted);
+            this.Name = "GronsfeldCipherForm";
+            this.Text = "Шифр Гронсфельда";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDecrypted;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Button buttonCypher;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label labelKey;
+    }
+}
+
---
+++ InformationSecurityLab7/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InformationSecurityLab7
+{
+    public partial class GronsfeldCipherForm : Form
+    {
+
+        public GronsfeldCipherForm()
+        {
+            InitializeComponent();
+        }
+
+        private string Cipher(char[] input, string key)
+        {
+            StringBuilder output = new StringBuilder();
+            int keyIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = input[i] - 'а';
+                int newIndex = (index + key[keyIndex] - '0') % 32;
+                output.Append((char)(newIndex + 'а'));
+                keyIndex++;
+
+                if (keyIndex >= key.Length)
+                    keyIndex = 0;
+            }
+
+            return output.ToString();
+        }
+
+        private void buttonCypher_Click(object sender, EventArgs e)
+        {
+            textBoxEncrypted.Text = Cipher(textBoxDecrypted.Text.ToLower().ToCharArray(), textBoxKey.Text);
+        }
+    }
+}
